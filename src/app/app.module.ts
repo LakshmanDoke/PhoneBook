27c81ffbe68365affc0cd365b5fd46015ca1f777
Lakshman Doke <lakshman.doke@godigit.com>
2022-04-03 15:02:43 +0530

    form created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AddContactComponent
+  },
+  {
+    path: 'contact',
+    component: ContactsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
